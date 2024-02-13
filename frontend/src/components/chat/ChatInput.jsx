cee9d48d9831990efbe30b80e15491399c6cacbc
Mrinal Deb <mrinal437a@gmail.com>
2024-02-13 12:42:22 +0530

    text input final iteration

--- frontend/src/components/chat/ChatInput.jsx
+++ frontend/src/components/chat/ChatInput.jsx
@@ -2,13 +2,14 @@ import "./chat.css"
 import SendIcon from '@mui/icons-material/Send';
 import  { useState } from 'react'
 import InputEmoji from 'react-input-emoji'
-import AddIcon from '@mui/icons-material/Add';
+import male from './images/male.png';
+import female from './images/female.png'
 
 const Chat = () => {
   const [text, setText] = useState('');
   const [name, setName] = useState('');
   const [step, setStep] = useState("join");
-  const avatars = [/* Array of image URLs */]; // Example: ["avatar1.png", "avatar2.png", "avatar3.png", "avatar4.png"]
+  const avatars = [male,female]; // Example: ["avatar1.png", "avatar2.png", "avatar3.png", "avatar4.png"]
 
   function handleOnEnter(text) {
     console.log('enter', text);
@@ -36,23 +37,25 @@ const Chat = () => {
   return (
     <div className="outer">
       {step === "join" && (
-        <button onClick={handleJoinClick}>Join Group Chat</button>
+        <button id="join-btn" onClick={handleJoinClick}>Join Group Chat</button>
       )}
       {step === "name" && (
+        <div id="name-div" >
         <form onSubmit={handleNameSubmit}>
-          <input
+          <input id="name-input"
             type="text"
             placeholder="Enter your name"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          <button type="submit">Submit</button>
+          <button id="name-btn" type="submit">Submit</button>
         </form>
+        </div>
       )}
       {step === "avatar" && (
         <>
-          <div>Choose Avatar:</div>
           <div className="avatar-container">
+            <h3>Choose Avatar</h3>
             {avatars.map((avatar, index) => (
               <img
                 key={index}
